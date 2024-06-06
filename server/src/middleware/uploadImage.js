3be80b9bd2f4bd8d767f7c5f04861bd82530f830
Pranjali-12 <kasbalepranjali@gmail.com>
2024-06-06 15:19:26 +0530

    middleware created

--- server/src/middleware/uploadImage.js
+++ server/src/middleware/uploadImage.js
@@ -1,26 +1,16 @@
-const multer = require('multer')
-const fs = require('fs');
+const multer = require('multer');
 const path = require('path');
 
-
-const uploadsDir = path.join(__dirname, '../../images');
-if (!fs.existsSync(uploadsDir)) {
-    fs.mkdirSync(uploadsDir);
-}
-
-
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadsDir);
-    },
-    filename: (req, file, cb) => {
-        const cgName = Date.now() + path.extname(file.originalname)
-        req.body.filename = cgName;
-        cb(null, cgName);
-    },
+  destination: (req, file, cb) => {
+      cb(null, 'images/');
+  },
+  filename: (req, file, cb) => {
+      cb(null, Date.now() + path.extname(file.originalname));
+  }
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage: storage });
 
 
 module.exports = upload;
--- server/src/routes/recipeRoutes.js
+++ server/src/routes/recipeRoutes.js
@@ -2,20 +2,9 @@ const {Router} = require('express');
 const {recipeController, filterController} = require('../controllers/recipeController');
 const passport = require('passport');
 const router = Router();
+const upload=require('../middleware/uploadImage')
 
-const multer = require('multer');
-const path = require('path');
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-      cb(null, 'images/');
-  },
-  filename: (req, file, cb) => {
-      cb(null, Date.now() + path.extname(file.originalname));
-  }
-});
-
-const upload = multer({ storage: storage });
 
 router.post("/addRecipe",passport.authenticate('jwt', { session: false }),upload.single('image'),recipeController.addRecipe);
 router.put("/updateRecipe/:id",passport.authenticate('jwt', { session: false }),recipeController.updateRecipe);

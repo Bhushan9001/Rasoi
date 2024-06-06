29e35648ba448fd3e59aed3ef0391e3b4b5fdb69
Bhushan <bhushanpawar9001@gmail.com>
2024-06-06 12:37:39 +0530

    Added get Requests for Recipe

--- server/src/routes/recipeRoutes.js
+++ server/src/routes/recipeRoutes.js
@@ -7,6 +7,8 @@ const router = Router();
 router.post("/addRecipe",passport.authenticate('jwt', { session: false }),upload.single('image'),recipeController.addRecipe);
 router.put("/updateRecipe/:id",passport.authenticate('jwt', { session: false }),recipeController.updateRecipe);
 router.delete("/deleteRecipe/:id",passport.authenticate('jwt',{session:false}),recipeController.deleteRecipe);
+router.get("/getRecipes",recipeController.getAllRecipe);
+router.get("/getRecipes/:id",recipeController.getRecipeById);
 
 //Filter Routes
 router.get("/userRecipes/:id",passport.authenticate('jwt',{session:false}),filterController.createdByUser);

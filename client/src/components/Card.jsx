cd4952b856af98cde073a8ba932f916809299c28
Pranjali-12 <kasbalepranjali@gmail.com>
2024-06-25 16:06:45 +0530

    all recipes page done

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -6,16 +6,20 @@ const Card = (props) => {
 
     return (
         <>
-            <div className='m-5 p-6 rounded-lg hover:cursor-pointer' style={{ boxShadow: 'rgba(0, 0, 0, 0.2) 0px 18px 50px -10px' }}>
-                <div>
-                    <img className='rounded-[3%]' src={props.img} alt={props.title} />
-                </div>
-                <div className='flex font-barlow-condensed items-center py-3 space-x-4'>
-                    <div className='text-3xl font-medium text-[#428C41]'>{props.title}</div>
-                    <div className='bg-[#68F665] px-3 py-2 rounded-lg font-poppins'>{props.cuisine}</div>
-                </div>
-                <div className='font-extralight font-ubuntu italic'>
-                    By {props.chef}
+            <div className='m-7 p-6 rounded-lg hover:cursor-pointer card ' style={{ boxShadow: 'rgba(0, 0, 0, 0.2) 0px 18px 50px -10px',transition: 'box-shadow 0.3s' }}
+            onMouseEnter={(e) => e.currentTarget.style.boxShadow = 'rgba(0, 0, 0, 0.4) 0px 24px 50px -12px'} 
+            onMouseLeave={(e) => e.currentTarget.style.boxShadow = 'rgba(0, 0, 0, 0.2) 0px 18px 50px -10px'}>
+                <div className='grid grid-cols-1 gap-3'>
+                    <div className='flex justify-center'>
+                        <img className='rounded-[3%] w-48 h-40' src={props.img} alt={props.title} />
+                    </div>
+                    <div className='flex justify-center items-center py-3 space-x-2'>
+                        <div className='text-3xl font-barlow-condensed font-medium text-[#428C41] text-center w-48 truncate text-wrap'>{props.title}</div>
+                        <div className='bg-[#68F665] px-3 py-2 rounded-full font-poppins inline-block mt-2 text-center w-28 truncate'>{props.cuisine}</div>
+                    </div>
+                    <div className='font-ubuntu italic text-center' style={{fontWeight:'100'}}>
+                        By {props.chef}
+                    </div>
                 </div>
             </div>
         </>
--- client/src/pages/Recipes.jsx
+++ client/src/pages/Recipes.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import Card from '../components/Card'
 import axios from 'axios'
+import Navbar from '../components/Navbar'
 
 const Recipes = () => {
-  const [recipes,setRecipes]=useState([])
-  useEffect(()=>{
+  const [recipes, setRecipes] = useState([])
+  useEffect(() => {
     const fetchRecipes = async () => {
       try {
         const response = await axios.get('http://localhost:8080/recipe/getRecipes');
@@ -18,24 +19,41 @@ const Recipes = () => {
     };
 
     fetchRecipes();
-  },[])
+  }, [])
 
   return (
     <>
-      <div className='ps-36 font-barlow-condensed text-4xl font-semibold py-4'>
+    <div className='py-5'>
+      <div className='text-5xl font-semibold font-barlow-condensed text-center py-5'>
         Delicious Discoveries
       </div>
       <div className='flex flex-wrap justify-center'>
-
         {
-          recipes && recipes.map((recipe)=>(
-            <Card img={recipe.imageurl} title={recipe.title} cuisine={recipe.cuisine} chef={recipe.authorName}/>
+          recipes && recipes.map((recipe) => (
+            <Card img={recipe.imageurl} title={recipe.title} cuisine={recipe.cuisine} chef={recipe.authorName} />
           ))
         }
-        
       </div>
-    </>
 
+      {/* <div className='ps-36 font-barlow-condensed text-4xl font-semibold py-4'>
+        Delicious Discoveries
+      </div>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 justify-center'>
+        {recipes && recipes.map((recipe) => {
+          const imgPath = recipe.imageurl.replace(/\\/g, '/');
+          return (
+            <Card 
+              key={recipe.title} 
+              img={imgPath} 
+              title={recipe.title} 
+              cuisine={recipe.cuisine} 
+              chef={recipe.authorName} 
+            />
+          );
+        })}
+      </div> */}
+    </div>
+    </>
   )
 }
 

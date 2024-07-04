fe8f2296e19773221e519d7e795cbf0cd067aa54
Pranjali-12 <kasbalepranjali@gmail.com>
2024-07-04 15:26:49 +0530

    bg image set

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,16 +4,17 @@ import Landing from './Landing'
 import Categories from '../components/Categories'
 import About from './About'
 import Footer from '../components/Footer'
+import landingBg from '../assets/landingBg.png'
 
 function HomePage() {
   return (
-    <>
+    <div className='bg-cover md:bg-center w-full bg-fixed' style={{ backgroundImage: `url(${landingBg})` }}>
       <Navbar/>
       <Landing/>
       <Categories/>
       <About/>
       <Footer/>
-    </>
+    </div>
   )
 }
 
